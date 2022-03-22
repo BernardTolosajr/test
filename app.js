61239357b5a9efff7bfdefd2daac92de9b41f42f
Ryan Arqueza <arquezaryan@gmail.com>
2022-03-22 09:54:21 +0800

    test

--- app.js
+++ app.js
@@ -29,67 +29,92 @@ const currentTimestamp = moment(date).format('YYYY-MM-DDTHH:mm:ss.SSSZ')
 
 // Auth API
 app.get('/auth/:authId', async (req, res) => {
-  const HTTP_METHOD = 'POST';
-  const HTTP_URI = '/v1/authorizations/applyToken.htm';
-  const CLIENT_ID = '2022030313304100083286';
-  const REQUEST_TIME = currentTimestamp;
-  const HTTP_BODY = {
+
+  const getSignature = (method, uri, clientId, httpBody) => {
+    const HTTP_METHOD = method;
+    const HTTP_URI = uri;
+    const CLIENT_ID = clientId;
+    const REQUEST_TIME = currentTimestamp;
+    const HTTP_BODY = httpBody
+  
+    const CONTENT_TO_BE_SIGNED = `${HTTP_METHOD} ${HTTP_URI}\n${CLIENT_ID}.${REQUEST_TIME}.${JSON.stringify(HTTP_BODY)}`;
+    // console.log(CONTENT_TO_BE_SIGNED);
+  
+        // set RSA privateKey
+    const rsaPrivateKey = rs.KEYUTIL.getKey(privateKey, 'passwd');
+  
+    // initialize
+    const sig = new rs.KJUR.crypto.Signature({alg: 'SHA256withRSA'});
+    
+    // initialize for signature generation
+    sig.init(rsaPrivateKey);
+    sig.updateString(CONTENT_TO_BE_SIGNED);
+    
+    // calculate signature
+    const sigValueHex = sig.sign();
+    // console.log('sigValueHex: ' + sigValueHex)
+  
+    const hex2b64Signature = rs.hex2b64(sigValueHex)
+    // console.log('hex2b64Signature: ' + hex2b64Signature)
+    
+    const encodedSignature = encodeURIComponent(hex2b64Signature)
+    // console.log('encodedSignature: ' + encodedSignature)
+
+    return "algorithm=RSA256, keyVersion=2, signature=" + encodedSignature;
+  }
+
+  // POST
+  // /v1/authorizations/applyToken.htm
+  // 2022030313304100083286
+ 
+  const body = 
+   {
     referenceClientId: '2022030313304100083286',
     grantType: 'AUTHORIZATION_CODE',
     authCode: req.params.authId,
     extendInfo: {customerBelongsTo:"GCASH"}
   };
 
-  const CONTENT_TO_BE_SIGNED = `${HTTP_METHOD} ${HTTP_URI}\n${CLIENT_ID}.${REQUEST_TIME}.${JSON.stringify(HTTP_BODY)}`;
-  console.log(CONTENT_TO_BE_SIGNED);
-
-    // let data = {
-    //   referenceClientId: '2022030313304100083286',
-    //   grantType: 'AUTHORIZATION_CODE',
-    //   authCode: req.params.authId,
-    //   extendInfo: JSON.stringify({ customerBelongsTo: 'GCASH' }),
-    // };
-
-      // set RSA privateKey
-  const rsaPrivateKey = rs.KEYUTIL.getKey(privateKey, 'passwd');
-
-  // initialize
-  const sig = new rs.KJUR.crypto.Signature({alg: 'SHA256withRSA'});
-  
-  // initialize for signature generation
-  sig.init(rsaPrivateKey);
-  sig.updateString(CONTENT_TO_BE_SIGNED);
-  
-  // calculate signature
-  const sigValueHex = sig.sign();
-  console.log('sigValueHex: ' + sigValueHex)
-
-  const hex2b64Signature = rs.hex2b64(sigValueHex)
-  console.log('hex2b64Signature: ' + hex2b64Signature)
-  
-  const encodedSignature = encodeURIComponent(hex2b64Signature)
-  console.log('encodedSignature: ' + encodedSignature)
-
   const headers = {
     "content-type": "application/json; charset=UTF-8",
-    "Client-Id": CLIENT_ID,
+    "Client-Id": '2022030313304100083286',
     "Request-Time": currentTimestamp,
-    "Signature": "algorithm=RSA256, keyVersion=2, signature=" + encodedSignature,
+    "Signature": getSignature('POST','/v1/authorizations/applyToken.htm', '2022030313304100083286', body ),
   }
 
 
     axios
-      .post('https://api-sit.saas.mynt.xyz/v1/authorizations/applyToken.htm', HTTP_BODY, { headers }) // v1
+      .post('https://api-sit.saas.mynt.xyz/v1/authorizations/applyToken.htm', body, { headers }) // v1
       .then((response) => {
+        // const BODY2 = {
+        //   accessToken: response.data.accessToken || "202203228xtJgyd7HXsrftcTE7SqLHRks12vtMd8MX07YHaV2xu0886400079501",
+        //   extendInfo: {customerBelongsTo:"GCASH"}
+        // }
+
+        // const headers2 = {
+        //   "content-type": "application/json; charset=UTF-8",
+        //   "Client-Id": '2022030313304100083286',
+        //   "Request-Time": currentTimestamp,
+        //   "Signature": getSignature('POST','/v1/customers/user/inquiryUserInfoByAccessToken.htm', '2022030313304100083286', BODY2 ),
+        // }
+
+        // console.log("BODY2:", BODY2)
+
+        // axios
+        // .post('https://api-sit.saas.mynt.xyz/v1/customers/user/inquiryUserInfoByAccessToken.htm', BODY2, {headers: headers2} ) // v1
+        // .then((response) => {
+        //   console.log(response.data)
+        // })
+
+
         return res.status(200).json({
           status: 'success',
           response: response.data,
-          CONTENT_TO_BE_SIGNED,
           authCode: req.params.authId,
           headers: {
             ...headers,
           },
-          HTTP_BODY,
+          // userData
         });
       })
       .catch((error) => {
@@ -98,12 +123,12 @@ app.get('/auth/:authId', async (req, res) => {
           status: 'fail',
           error: `GCASH SERVER ERROR: ${error.message}`,
           errors: error,
-          CONTENT_TO_BE_SIGNED,
-          authCode: req.params.authId,
-          headers: {
-            ...headers,
-          },
-          HTTP_BODY,
+          // CONTENT_TO_BE_SIGNED,
+          // authCode: req.params.authId,
+          // headers: {
+          //   ...headers,
+          // },
+          // HTTP_BODY,
         });
       });
   // });
